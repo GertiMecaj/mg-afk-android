4aa9c06faab5fe0b2aff7a970867e6494f0345c2
GertiMecaj <102616177+GertiMecaj@users.noreply.github.com>
2026-09-23 02:45:53 +0200

    Bump version to 2.4.25

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.mgafk.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 69
-        versionName = "2.4.24"
+        versionCode = 70
+        versionName = "2.4.25"
     }
 
     buildTypes {
